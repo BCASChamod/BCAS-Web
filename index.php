a11a952bb10a0b6a432726bf2f9307fbf2ffd5c3
BCASChamod <chamod@bcas.lk>
2025-06-16 17:57:06 +0530

    Timeline Developed

--- index.php
+++ index.php
@@ -47,6 +47,96 @@
       </div>
     </section>
 
+    <section class="awards-section">
+      <h2 class="timeline-header">
+        Consistently Recognized for Excellence in Education
+      </h2>
+      <div id="timeline" class="award-timeline">
+        <svg id="Timeline" class="timeline-svg" style="width: 3444px; height: 600px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3000 391.43">
+          <g id="item-up" class="timeline-item">
+            <path id="Pointer" class="svg-inneritem" d="M82.52,217.37h-10V69.99c0-2.76,2.24-5,5-5h0c2.76,0,5,2.24,5,5v147.38ZM82.52,217.37h-10l5,7.62,5-7.62Z"/>
+            <text class="svg-innertext" transform="translate(38.69 259.31)"><tspan x="0" y="0">2000</tspan></text>
+            <text class="svg-innersubtext" transform="translate(93.2 75.22)"><tspan x="0" y="0">Founding of BCAS</tspan></text>
+          </g>
+          <g id="item-down" class="timeline-item">
+            <path id="Pointer-2" data-name="Pointer" class="svg-inneritem" d="M292.71,178.37h10v147.38c0,2.76-2.24,5-5,5h0c-2.76,0-5-2.24-5-5v-147.38ZM292.71,178.37h10s-5-7.62-5-7.62l-5,7.62Z"/>
+            <text class="svg-innertext" transform="translate(263.09 167.43)"><tspan x="0" y="0">2010</tspan></text>
+            <text class="svg-innersubtext" transform="translate(133.68 290.82)"><tspan x="0" y="0">Performance excellence</tspan><tspan x="26.42" y="16.8">The fastest growing</tspan><tspan x="-18.79" y="33.6">Edexcel Center in Sri Lanka</tspan></text>
+          </g>
+          <g id="item-up-2" data-name="item-up" class="timeline-item">
+            <path id="Pointer-3" data-name="Pointer" class="svg-inneritem" d="M420.14,217.37h-10V69.99c0-2.76,2.24-5,5-5h0c2.76,0,5,2.24,5,5v147.38ZM420.14,217.37h-10l5,7.62,5-7.62Z"/>
+            <text class="svg-innertext" transform="translate(384.74 259.31)"><tspan x="0" y="0">2011</tspan></text>
+            <text class="svg-innersubtext" transform="translate(431.52 75.22)"><tspan x="0" y="0">Best performance in </tspan><tspan x="0" y="16.8">Sri Lanka in 2011 by </tspan><tspan x="0" y="33.6">Edexcel - UK</tspan></text>
+          </g>
+          <g id="item-down-2" data-name="item-down" class="timeline-item">
+            <path id="Pointer-4" data-name="Pointer" class="svg-inneritem" d="M613.27,178.37h10v147.38c0,2.76-2.24,5-5,5h0c-2.76,0-5-2.24-5-5v-147.38ZM613.27,178.37h10s-5-7.62-5-7.62l-5,7.62Z"/>
+            <text class="svg-innertext" transform="translate(582.41 167.43)"><tspan x="0" y="0">2013 </tspan></text>
+            <text class="svg-innersubtext" transform="translate(632.42 310.56)"><tspan x="0" y="0">“BTEC Gold Partner” </tspan><tspan x="0" y="16.8">presented by Edexcel - UK</tspan></text>
+          </g>
+          <g id="item-up-3" data-name="item-up">
+            <path id="Pointer-5" data-name="Pointer" class="svg-inneritem" d="M843.95,215.8h-10V11.27c0-2.76,2.24-5,5-5h0c2.76,0,5,2.24,5,5v204.52ZM843.95,215.8h-10l5,10.48,5-10.48Z"/>
+            <text class="svg-innertext" transform="translate(805.2 259.31)"><tspan x="0" y="0">2014</tspan></text>
+            <text class="svg-innersubtext" transform="translate(855.66 25.66)"><tspan x="0" y="0">The Education Service Excellence </tspan><tspan x="0" y="16.8">Award by United Nations Decade </tspan><tspan x="0" y="33.6">of Education for sustainable </tspan><tspan x="0" y="50.4">Development (UN-DESD)</tspan></text>
+            <text class="svg-innersubtext" transform="translate(855.66 122.02)"><tspan x="0" y="0">Recognition by LMD Magazine as the </tspan><tspan x="0" y="16.8">‘Number-1 private higher education </tspan><tspan x="0" y="33.6">institute’</tspan></text>
+            <path class="svg-separator {" d="M856,92.02h226.75c.77,0,1.4.63,1.4,1.4h0c0,.77-.63,1.4-1.4,1.4h-226.75v-2.8h0Z"/>
+          </g>
+          <g id="item-down-3" data-name="item-down" class="timeline-item">
+            <path id="Pointer-6" data-name="Pointer" class="svg-inneritem" d="M1140.14,178.37h10v147.38c0,2.76-2.24,5-5,5h0c-2.76,0-5-2.24-5-5v-147.38ZM1140.14,178.37h10s-5-7.62-5-7.62l-5,7.62Z"/>
+            <text class="svg-innertext" transform="translate(1112.06 167.43)"><tspan x="0" y="0">2015</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1159.29 290.56)"><tspan x="0" y="0">Platinum Partner </tspan><tspan x="0" y="16.8">No.1 BTEC Centre</tspan><tspan x="0" y="33.6">in Sri Lanka</tspan></text>
+          </g>
+          <g id="item-up-4" data-name="item-up" class="timeline-item">
+            <path id="Pointer-7" data-name="Pointer" class="svg-inneritem" d="M1346.52,214.42h-10V7.96c0-2.76,2.24-5,5-5h0c2.76,0,5,2.24,5,5v206.46ZM1346.52,214.42h-10l5,10.57,5-10.57Z"/>
+            <text class="svg-innertext" transform="translate(1308.28 259.31)"><tspan x="0" y="0">2016</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1357.89 75.22)"><tspan x="0" y="0">Visionary Leadership Award </tspan><tspan x="0" y="16.8">for BCAS Chairman BCAS</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1357.89 21.73)"><tspan x="0" y="0">Platinum Partner No.1 BTEC </tspan><tspan x="0" y="16.8">Centre in Sri Lanka</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1357.89 126.16)"><tspan x="0" y="0">Educational Institute with Best</tspan><tspan x="0" y="16.8">Academic &amp; Industry Interface</tspan></text>
+            <path class="svg-separator {" d="M1358.62,103.31h195.29c.77,0,1.4.63,1.4,1.4h0c0,.77-.63,1.4-1.4,1.4h-195.29v-2.8h0Z"/>
+            <path class="svg-separator {" d="M1358.62,50.46h195.29c.77,0,1.4.63,1.4,1.4h0c0,.77-.63,1.4-1.4,1.4h-195.29v-2.8h0Z"/>
+          </g>
+          <g id="item-down-4" data-name="item-down" class="timeline-item">
+            <path id="Pointer-8" data-name="Pointer" class="svg-inneritem" d="M1615.58,181.26h10v205.16c0,2.76-2.24,5-5,5h0c-2.76,0-5-2.24-5-5v-205.16ZM1615.58,181.26h10s-5-10.51-5-10.51l-5,10.51Z"/>
+            <text class="svg-innertext" transform="translate(1588.09 167.43)"><tspan x="0" y="0">2017</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1634.73 346.56)"><tspan x="0" y="0">Best Employer Branch Award</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1634.73 381.34)"><tspan x="0" y="0">Gold Award Pearson, UK</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1634.73 265.39)"><tspan x="0" y="0">Excellence in Training Award </tspan><tspan x="0" y="16.8">(Overall Award based on results </tspan><tspan x="0" y="33.6">based training Asia Pacific HRM </tspan><tspan x="0" y="50.4">Congress)</tspan></text>
+            <path class="svg-separator {" d="M1635.79,325.64h195.29c.77,0,1.4.63,1.4,1.4h0c0,.77-.63,1.4-1.4,1.4h-195.29v-2.8h0Z"/>
+            <path class="svg-separator {" d="M1635.79,357.31h195.29c.77,0,1.4.63,1.4,1.4h0c0,.77-.63,1.4-1.4,1.4h-195.29v-2.8h0Z"/>
+          </g>
+          <g id="item-up-5" data-name="item-up" class="timeline-item">
+            <path id="Pointer-9" data-name="Pointer" class="svg-inneritem" d="M1908.56,214.42h-10V7.96c0-2.76,2.24-5,5-5h0c2.76,0,5,2.24,5,5v206.46ZM1908.56,214.42h-10l5,10.57,5-10.57Z"/>
+            <text class="svg-innertext" transform="translate(1870.47 259.31)"><tspan x="0" y="0">2018</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1919.94 11.73)"><tspan x="0" y="0">Best Employer Brand Award, </tspan><tspan x="0" y="16.8">Outstanding Contribution </tspan><tspan x="0" y="33.6">to the cause of Education, </tspan><tspan x="0" y="50.4">Award for Excellence in Training</tspan><tspan x="0" y="67.2">(World HRD Congress 13th Employers </tspan><tspan x="0" y="84">Branding Award Le Meridian, Singapore)</tspan></text>
+            <text class="svg-innersubtext" transform="translate(1919.94 126.16)"><tspan x="0" y="0">Gold Award Pearson, UK</tspan></text>
+            <path class="svg-separator {" d="M1920.65,106.31h258.6c.77,0,1.4.63,1.4,1.4h0c0,.77-.63,1.4-1.4,1.4h-258.6v-2.8h0Z"/>
+          </g>
+          <g id="item-down-5" data-name="item-down" class="timeline-item">
+            <path id="Pointer-10" data-name="Pointer" class="svg-inneritem" d="M2255.5,175.73h10v94.53c0,2.76-2.24,5-5,5h0c-2.76,0-5-2.24-5-5v-94.53ZM2255.5,175.73h10s-5-4.98-5-4.98l-5,4.98Z"/>
+            <text class="svg-innertext" transform="translate(2227.26 167.43)"><tspan x="0" y="0">2019</tspan></text>
+            <text class="svg-innersubtext" transform="translate(2276.37 253.33)"><tspan x="0" y="0">Gold Award </tspan><tspan x="0" y="16.8">Pearson, UK</tspan></text>
+          </g>
+          <g id="item-up-6" data-name="item-up" class="timeline-item">
+            <path id="Pointer-11" data-name="Pointer" class="svg-inneritem" d="M2455.44,220.91h-10v-76.61c0-2.76,2.24-5,5-5h0c2.76,0,5,2.24,5,5v76.61ZM2455.44,220.91h-10l5,4.08,5-4.08Z"/>
+            <text class="svg-innertext" transform="translate(2413.03 259.31)"><tspan x="0" y="0">2020</tspan></text>
+            <text class="svg-innersubtext" transform="translate(2468.24 150.28)"><tspan x="0" y="0">Gold Award </tspan><tspan x="0" y="16.8">Pearson, UK</tspan></text>
+          </g>
+          <g id="item-down-6" data-name="item-down" class="timeline-item">
+            <path id="Pointer-12" data-name="Pointer" class="svg-inneritem" d="M2635.16,175.73h10v94.53c0,2.76-2.24,5-5,5h0c-2.76,0-5-2.24-5-5v-94.53ZM2635.16,175.73h10s-5-4.98-5-4.98l-5,4.98Z"/>
+            <text class="svg-innertext" transform="translate(2604.18 167.43)"><tspan x="0" y="0">2022</tspan></text>
+            <text class="svg-innersubtext" transform="translate(2656.03 253.33)"><tspan x="0" y="0">Gold Award </tspan><tspan x="0" y="16.8">Pearson, UK</tspan></text>
+          </g>
+          <g id="item-up-7" data-name="item-up" class="timeline-item">
+            <path id="Pointer-13" data-name="Pointer" class="svg-inneritem" d="M2812.67,220.91h-10v-76.61c0-2.76,2.24-5,5-5h0c2.76,0,5,2.24,5,5v76.61ZM2812.67,220.91h-10l5,4.08,5-4.08Z"/>
+            <text class="svg-innertext" transform="translate(2771.8 259.31)"><tspan x="0" y="0">2023</tspan></text>
+            <text class="svg-innersubtext" transform="translate(2825.48 150.28)"><tspan x="0" y="0">Gold Award </tspan><tspan x="0" y="16.8">Pearson, UK</tspan></text>
+          </g>
+          <g id="main-branch">
+            <path class="main-branch" d="M3000,207.47H5c-2.76,0-5-2.24-5-5h0c0-2.76,2.24-5,5-5h2995v10Z"/>
+          </g>
+        </svg>
+      </div>
+    </section>
+
     <section class="dummy"></section>
   </main>
 
